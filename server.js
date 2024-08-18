020b103b6c9ca0ff94f16010c9b3cb6a5068512b
DanielGCG <danielgcg@dcc.ufrj.br>
2024-08-18 15:26:43 -0300

    Update server.js

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ const server = http.createServer((req, res) => {
 // Define a porta em que o servidor vai escutar
 const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor rodando em http://boteco.live:${PORT}`);
 });
