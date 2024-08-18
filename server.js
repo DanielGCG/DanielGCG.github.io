327955bbf2b48c6e66f15287fd0996559e9cfa5b
DanielGCG <danielgcg@dcc.ufrj.br>
2024-08-18 15:39:05 -0300

    b

--- server.js
+++ server.js
@@ -4,18 +4,37 @@ const path = require('path');
 
 // Cria um servidor HTTP
 const server = http.createServer((req, res) => {
-  // Define o caminho absoluto do arquivo index.html
-  const filePath = path.join(__dirname, 'index.html');
+  // Define o caminho absoluto do arquivo solicitado
+  let filePath = path.join(__dirname, 'index.html');
 
-  // Lê o arquivo index.html
+  // Para melhorar, poderíamos verificar o req.url para servir outros arquivos
+  if (req.url !== '/' && req.url !== '/favicon.ico') {
+    filePath = path.join(__dirname, req.url);
+  }
+
+  // Lê o arquivo solicitado
   fs.readFile(filePath, (err, content) => {
     if (err) {
-      // Em caso de erro, retorna uma mensagem de erro
-      res.writeHead(500);
-      res.end('Erro ao carregar a página.');
+      // Verifica se o erro é por arquivo não encontrado
+      if (err.code === 'ENOENT') {
+        res.writeHead(404);
+        res.end('Página não encontrada');
+      } else {
+        // Em caso de outro erro, retorna um erro 500
+        res.writeHead(500);
+        res.end('Erro ao carregar a página.');
+      }
     } else {
-      // Caso contrário, envia o conteúdo do index.html
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+      // Define o tipo de conteúdo de acordo com o arquivo
+      let contentType = 'text/html';
+      if (filePath.endsWith('.css')) {
+        contentType = 'text/css';
+      } else if (filePath.endsWith('.js')) {
+        contentType = 'application/javascript';
+      }
+
+      // Caso contrário, envia o conteúdo do arquivo solicitado
+      res.writeHead(200, { 'Content-Type': contentType });
       res.end(content);
     }
   });
@@ -24,5 +43,5 @@ const server = http.createServer((req, res) => {
 // Define a porta em que o servidor vai escutar
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Servidor rodando em http://boteco.live:${PORT}`);
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
