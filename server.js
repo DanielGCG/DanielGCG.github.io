0c2d5a09e99234d22f41bbd50e8929de1e52982a
DanielGCG <danielgcg@dcc.ufrj.br>
2024-08-18 22:34:29 -0300

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Endpoint para enviar as configurações do Firebase ao frontend
 app.get('/firebase-config', (req, res) => {
     res.json({
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID
+        apiKey: process.env.FB_apiKey,
+        authDomain: process.env.FB_authDomain,
+        projectId: process.env.FB_projectId,
+        storageBucket: process.env.FB_storageBucket,
+        messagingSenderId: process.env.FB_messagingSenderId,
+        appId: process.env.FB_appId
     });
 });
 
